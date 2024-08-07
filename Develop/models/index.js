6f762346d0fccfa00e71e3793df9356baa06bd4d
AlejandroM2023 <alejandromeza437@gmail.com>
2024-08-07 17:22:02 -0500

    iassociations in models/index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Tag,
+  {thorugh: 'ProductTag'}
+);
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product,{thorugh: 'ProductTag'});
 
 module.exports = {
   Product,
